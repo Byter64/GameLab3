5a5682016455a5f7f11a3afc21939d2cd3643010
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2023-12-04 16:08:39 +0100

    Changed: Increased max entities from 1,000 to 10,000

--- code/engine/src/EntityComponentSystem/Entity.h
+++ code/engine/src/EntityComponentSystem/Entity.h
@@ -7,6 +7,6 @@ namespace Engine
 {
     typedef std::uint32_t Entity;
     typedef std::bitset<MAX_COMPONENTS> Signature;
-    const Entity MAX_ENTITIES = 1000;
+    const Entity MAX_ENTITIES = 10000;
     const Entity INVALID_ENTITY_ID = MAX_ENTITIES;
 } // Engine
