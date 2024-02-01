95ffc8a38f6ae6c0a4ea56708e3ca16f38ab7f20
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-02-01 12:50:52 +0100

    Fixed: Rotation is now normalized before being applied

--- code/engine/src/ECS/Components/Transform.cpp
+++ code/engine/src/ECS/Components/Transform.cpp
@@ -60,7 +60,7 @@ namespace Engine
 
     void Transform::SetRotation(const glm::quat &rotation)
     {
-        this->rotation = rotation;
+        this->rotation = glm::normalize(rotation);
         hasTransformChanged = true;
 
         SetIsGlobalTranslationOutdated();
@@ -74,7 +74,6 @@ namespace Engine
             glm::mat4 scale = glm::scale(glm::mat4(1.0f), this->scale);
             glm::mat4 rotation = glm::toMat4(this->rotation);
             glm::mat4 translation = glm::translate(glm::mat4(1.0f), this->translation);
-
 
             matrix = translation * rotation * scale;
             hasTransformChanged = false;
