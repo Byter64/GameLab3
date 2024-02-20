a9df401e704bf0691389aef09f8b5695f408a9a7
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-02-20 09:50:36 +0100

    Revert "Added: Debug shit, need to revert this later"
    This reverts commit b20f5afffca8aa1b0512f30376c04c240ea50295.

--- code/engine/include/ECS/Systems/TransformParentSystem.h
+++ code/engine/include/ECS/Systems/TransformParentSystem.h
@@ -11,8 +11,6 @@ namespace Engine
     public:
         void EntityAdded(Entity entity) override;
         void EntityRemoved(Entity entity) override;
-
-        void Print();
     };
 
 } // Engine
--- code/engine/include/EntityUtility.h
+++ code/engine/include/EntityUtility.h
@@ -9,7 +9,7 @@
 namespace Engine
 {
     Entity FindChild(Entity root, Name name);
-    Entity CopyEntity(Entity entity, bool copyChildren, int count);
+    Entity CopyEntity(Entity entity, bool copyChildren);
     std::vector<Entity> ImportGLTF(std::filesystem::path gltf, bool addParent = false);
     Entity SpawnBullet(Entity spawner, glm::vec3 position, glm::vec3 direction, float speed = 4.0f);
     Entity SpawnEnemy(std::pair<int, int> startPos, Engine::EnemyBehaviour::Behaviour behaviour);
