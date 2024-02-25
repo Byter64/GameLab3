0a3a0409fe05a3f2eddca2765b38969cab554d02
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-02-25 17:25:02 +0100

    Added: function to get all components of a certain type within a hierarchy

--- code/engine/include/EntityUtility.h
+++ code/engine/include/EntityUtility.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include "glm/glm.hpp"
 #include "ECS/Components/EnemyBehaviour.h"
+#include "ECSSystem.h"
+#include "ECS/Components/Transform.h"
 
 namespace Engine
 {
@@ -15,4 +17,22 @@ namespace Engine
     Entity SpawnEnemy(std::pair<int, int> startPos, Engine::EnemyBehaviour::Behaviour behaviour);
     Entity SpawnWall(glm::vec3 position);
     void RemoveEntityWithChildren(Entity entity, bool removeParent = true);
+
+    template<typename T>
+    std::vector<T*> GetComponentsInChildren(Entity entity)
+    {
+        std::vector<T*> components;
+
+        if(ecsSystem->HasComponent<T>(entity))
+            components.push_back(&ecsSystem->GetComponent<T>(entity));
+        Transform& transform = ecsSystem->GetComponent<Transform>(entity);
+        for(Transform* childTransform : transform.GetChildren())
+        {
+            Entity child = ecsSystem->GetEntity(*childTransform);
+            std::vector<T*> childComponents = GetComponentsInChildren<T>(child);
+            components.insert(components.end(), childComponents.begin(), childComponents.end());
+        }
+
+        return components;
+    }
 } // Engine
