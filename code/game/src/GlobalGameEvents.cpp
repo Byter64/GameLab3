e1f415a8f138b123a93308e274af6d00913cfb69
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-03-05 16:53:40 +0100

    Removed: Old Demo

--- code/game/src/GlobalGameEvents.cpp
+++ code/game/src/GlobalGameEvents.cpp
@@ -12,9 +12,6 @@ extern std::shared_ptr<Engine::DungeonSystem> dungeonSystem;
 extern std::shared_ptr<Engine::TextRenderSystem> textRenderSystem;
 extern GLFWwindow *window;
 
-Engine::Entity krawatterich;
-Engine::Entity root;
-
 void Engine::OnStartGame(int screenWidth, int screenHeight)
 {
     inputSystem = new Engine::InputSystem(window);
@@ -54,60 +51,4 @@ void Engine::OnEndGame()
 void Engine::Update(float deltaTime)
 {
 
-}
-
-glm::vec3 movement;
-
-void MovementXY(void*, glm::vec2 input)
-{
-    movement.x = input.x;
-    movement.y = input.y;
-
-    if(movement != glm::vec3(0,0,0))
-        movement = glm::normalize(movement) * 8.0f;
-}
-
-void MovementZ(void*, glm::vec2 input)
-{
-    movement.z = input.x;
-
-    if(movement != glm::vec3(0,0,0))
-        movement = glm::normalize(movement) * 8.0f;
-}
-
-void LoadDemo()
-{
-
-    std::shared_ptr<Engine::InputActionVec2> movementXY = std::make_shared<Engine::InputActionVec2>("MovementXY");
-    movementXY->AddKeyboardBinding(GLFW_KEY_A, GLFW_KEY_D, GLFW_KEY_W, GLFW_KEY_S);
-    movementXY->AddOnValueChange(nullptr, MovementXY);
-
-    inputSystem->Add(movementXY);
-
-
-    std::shared_ptr<Engine::InputActionVec2> movementZ = std::make_shared<Engine::InputActionVec2>("MovementZ");
-    movementZ->AddKeyboardBinding(GLFW_KEY_E, GLFW_KEY_Q, 0, 0);
-    movementZ->AddOnValueChange(nullptr, MovementZ);
-    inputSystem->Add(movementZ);
-
-    std::filesystem::path path = "C:/Users/Yanni/Desktop/Fliegengesicht.gltf";
-    root = ecsSystem->CreateEntity();
-    ecsSystem->AddComponent<Engine::Name>(root, "Root");
-    ecsSystem->AddComponent<Engine::Transform>(root, Engine::Transform());
-
-    for(Engine::Entity entity : Engine::ImportGLTF(path))
-    {
-        ecsSystem->GetComponent<Engine::Transform>(entity).SetParent(&ecsSystem->GetComponent<Engine::Transform>(root));
-    }
-    krawatterich = Engine::FindChild(root, "Krawatterich");
-
-    Engine::BoxCollider& boxCollider = ecsSystem->AddComponent<Engine::BoxCollider>(krawatterich);
-
-    GLuint vertexColorshader = renderSystem->LoadShader(Engine::Files::ASSETS / "Shaders/Default/FS_Default_VertexColor.frag", GL_FRAGMENT_SHADER);
-    Engine::Entity vertexPaintedSphere1 = Engine::FindChild(root, "VertexPaintedSphere");
-    ecsSystem->GetComponent<Engine::MeshRenderer>(vertexPaintedSphere1).primitiveData[0].shader.SetFragmentShader
-    (vertexColorshader);
-    Engine::Entity vertexPaintedSphere2 = Engine::FindChild(root, "VertexPaintedSphere.001");
-    ecsSystem->GetComponent<Engine::MeshRenderer>(vertexPaintedSphere2).primitiveData[0].shader.SetFragmentShader
-    (vertexColorshader);
 }
