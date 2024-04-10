8e200345256a6e9d8c4bba54fb6c47b547d6b191
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-04-10 22:05:22 +0200

    Added: Transform children to ECSException.errorDescription

--- code/engine/src/ECSException.cpp
+++ code/engine/src/ECSException.cpp
@@ -1,6 +1,7 @@
 #include "ECSException.h"
 #include "ECSSystem.h"
 #include "ECS/Components/Name.h"
+#include "ECS/Components/Transform.h"
 
 namespace Engine
 {
@@ -26,6 +27,21 @@ namespace Engine
             }
         }
         errorDescription.pop_back();
+
+        if(ecsSystem->HasComponent<Transform>(entity))
+        {
+            Transform& transform = ecsSystem->GetComponent<Transform>(entity);
+            errorDescription += "Transform has " + std::to_string(transform.GetChildren().size()) + " children:";
+            for(int i = 0; i < transform.GetChildren().size(); i++)
+            {
+                Engine::Entity childEntity = ecsSystem->GetEntity(transform.GetChild(i));
+                errorDescription += "\n\t";
+                if(ecsSystem->HasComponent<Name>(childEntity))
+                    errorDescription += ecsSystem->GetComponent<Name>(childEntity);
+                else
+                    errorDescription += "Without name";
+            }
+        }
 
         this->errorDescription = errorDescription;
     }
