b962bfd31b404e38a1f13d799b38a10ec423ed90
Yannik Stamm <yannik.stamm@stud-mail.uni-wuerzburg.de>
2024-06-19 22:28:20 +0200

    Added: Keyboard for controller

--- code/game/include/Keyboard.h
+++ code/game/include/Keyboard.h
@@ -3,21 +3,54 @@
 
 class Keyboard
 {
-    int pos = 0;
     std::string text = "";
     int scale = 1;
     int charsPerLine = 7;
+    int xPixel;
+    int yPixel;
+    glm::vec2 position;
 
     std::map<char, Engine::Entity> keys;
+
+    int selectorPos = 0;
+    Engine::Entity selector;
+
+    std::shared_ptr<Engine::InputActionButton> up;
+    std::shared_ptr<Engine::InputActionButton> down;
+    std::shared_ptr<Engine::InputActionButton> left;
+    std::shared_ptr<Engine::InputActionButton> right;
+    std::shared_ptr<Engine::InputActionButton> enter;
+    std::shared_ptr<Engine::InputActionButton> del;
+
+    static void OnUp(void* o);
+    static void OnDown(void* o);
+    static void OnLeft(void* o);
+    static void OnRight(void* o);
+    static void OnEnter(void* o);
+    static void OnDelete(void* o);
 public:
-    explicit Keyboard(glm::vec2 position, int scale = 3);
+    explicit Keyboard(glm::vec2 position, int scale = 3, int charsPerLine = 7);
     explicit Keyboard(std::string startText, int charsPerLine, glm::vec2 position, int scale = 3);
+    ~Keyboard();
 
     std::string GetText();
     void SetPos(int x, int y);
     void AddPos(int x, int y);
     void EnterChar();
-
-    //Always deletes lastly entered char
+    //Always deletes the lastly entered char
     void DeleteChar();
+
+    //Sets the given action as up. This action will be cleaned up on ~Keyboard()
+    void SetUpAction(std::shared_ptr<Engine::InputActionButton> action);
+    //Sets the given action as down. This action will be cleaned up on ~Keyboard()
+    void SetDownAction(std::shared_ptr<Engine::InputActionButton> action);
+    //Sets the given action as left. This action will be cleaned up on ~Keyboard()
+    void SetLeftAction(std::shared_ptr<Engine::InputActionButton> action);
+    //Sets the given action as right. This action will be cleaned up on ~Keyboard()
+    void SetRightAction(std::shared_ptr<Engine::InputActionButton> action);
+    //Sets the given action as enter. This action will be cleaned up on ~Keyboard()
+    void SetEnterAction(std::shared_ptr<Engine::InputActionButton> action);
+    //Sets the given action as delete. This action will be cleaned up on ~Keyboard()
+    void SetDeleteAction(std::shared_ptr<Engine::InputActionButton> action);
+
 };
--- code/game/src/Keyboard.cpp
+++ code/game/src/Keyboard.cpp
@@ -1,18 +1,25 @@
-//
-// Created by Yannik Stamm on 19.06.2024.
-//
-
 #include "Keyboard.h"
+#include "ECSExtension.h"
 
-
-Keyboard::Keyboard(glm::vec2 position, int scale)
+Keyboard::Keyboard(glm::vec2 position, int scale, int charsPerLine)
 {
+    this->position = position;
     this->scale = scale;
+    this->charsPerLine = charsPerLine;
+    xPixel = scale * 16;
+    yPixel = scale * 16;
+
+    selector = Engine::ImportGLTF(Engine::Files::ASSETS / "Graphics/Models/Selector.glb")[0];
+    ecsSystem->GetComponent<Engine::Transform>(selector).SetRotation(glm::vec3(glm::radians(90.0f), 0, 0));
+    ecsSystem->GetComponent<Engine::Transform>(selector).SetScale(glm::vec3(xPixel));
+    ecsSystem->GetComponent<Engine::MeshRenderer>(selector).renderLayer = Engine::UI;
+    GLuint shader = Engine::Systems::renderSystem->LoadShader(Engine::Files::ASSETS / "Shaders/Default/VS_UI_Default.vert", GL_VERTEX_SHADER);
+    for(auto const& renderer : Engine::GetComponentsInChildren<Engine::MeshRenderer>(selector))
+        renderer->SetVertexShader(shader);
+
 
     int i = 0;
     int lines = (int)::ceilf(26.0f / charsPerLine);
-    int xPixel = scale * ???;
-    int yPixel = scale * ???;
     for(int y = 0; y < lines; y++)
         for(int x = 0; x < charsPerLine; x++)
         {
@@ -21,16 +28,29 @@ Keyboard::Keyboard(glm::vec2 position, int scale)
             text.SetText(std::string("") + (char)('a' + i));
             text.scale = scale;
             text.position = position + glm::vec2(xPixel * x, yPixel * y);
-
+            text.horizontalAlignment = Engine::Text::Center;
+            text.verticalAlignment = Engine::Text::Center;
             i++;
         }
+
+    SetPos(0, 0);
 }
 
-Keyboard::Keyboard(std::string startText, int charsPerLine, glm::vec2 position, int scale) : Keyboard(position, scale)
+Keyboard::Keyboard(std::string startText, int charsPerLine, glm::vec2 position, int scale) : Keyboard(position, scale, charsPerLine)
 {
     text = startText;
-    this->charsPerLine = charsPerLine;
 }
+
+Keyboard::~Keyboard()
+{
+    Engine::Systems::inputSystem->Remove(up);
+    Engine::Systems::inputSystem->Remove(down);
+    Engine::Systems::inputSystem->Remove(left);
+    Engine::Systems::inputSystem->Remove(right);
+    Engine::Systems::inputSystem->Remove(enter);
+    Engine::Systems::inputSystem->Remove(del);
+}
+
 
 std::string Keyboard::GetText()
 {
@@ -39,22 +59,105 @@ std::string Keyboard::GetText()
 
 void Keyboard::SetPos(int x, int y)
 {
-    pos = x + y * charsPerLine;
-    pos = std::clamp(pos, 0, 26);
+    selectorPos = x + y * charsPerLine;
+    selectorPos = std::clamp(selectorPos, 0, 25);
+
+    ecsSystem->GetComponent<Engine::Transform>(selector).SetTranslation(glm::vec3(position, 0) + glm::vec3(x * xPixel, y * yPixel, 0));
 }
 
 void Keyboard::AddPos(int x, int y)
 {
-    pos += x + y * charsPerLine;
-    pos = std::clamp(pos, 0, 26);
+    selectorPos += x + y * charsPerLine;
+    selectorPos = std::clamp(selectorPos, 0, 25);
+
+    ecsSystem->GetComponent<Engine::Transform>(selector).SetTranslation(glm::vec3(position, 0) + glm::vec3((selectorPos % charsPerLine) * xPixel, (selectorPos / charsPerLine) * yPixel, 0));
 }
 
 void Keyboard::EnterChar()
 {
-    text += (char)('a' + pos);
+    text += (char)('a' + selectorPos);
 }
 
 void Keyboard::DeleteChar()
 {
     text.pop_back();
+}
+
+void Keyboard::SetUpAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    up = action;
+    up->AddOnStart(this, OnUp);
+    Engine::Systems::inputSystem->Add(up);
+}
+
+void Keyboard::SetDownAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    down = action;
+    down->AddOnStart(this, OnDown);
+    Engine::Systems::inputSystem->Add(down);
+}
+
+void Keyboard::SetLeftAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    left = action;
+    left->AddOnStart(this, OnLeft);
+    Engine::Systems::inputSystem->Add(left);
+}
+
+void Keyboard::SetRightAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    right = action;
+    right->AddOnStart(this, OnRight);
+    Engine::Systems::inputSystem->Add(right);
+}
+
+void Keyboard::SetEnterAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    enter = action;
+    enter->AddOnStart(this, OnEnter);
+    Engine::Systems::inputSystem->Add(enter);
+}
+
+void Keyboard::SetDeleteAction(std::shared_ptr<Engine::InputActionButton> action)
+{
+    del = action;
+    del->AddOnStart(this, OnDelete);
+    Engine::Systems::inputSystem->Add(del);
+}
+
+
+void Keyboard::OnUp(void* o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->AddPos(0, -1);
+}
+
+void Keyboard::OnDown(void* o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->AddPos(0, 1);
+}
+
+void Keyboard::OnLeft(void* o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->AddPos(-1, 0);
+}
+
+void Keyboard::OnRight(void* o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->AddPos(1, 0);
+}
+
+void Keyboard::OnEnter(void *o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->EnterChar();
+}
+
+void Keyboard::OnDelete(void *o)
+{
+    Keyboard* keyboard = static_cast<Keyboard*>(o);
+    keyboard->DeleteChar();
 }
